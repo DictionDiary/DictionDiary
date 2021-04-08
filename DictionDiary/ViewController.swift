bbb228b0519753dffb0a01065e438538e7a26b86
Mikayla <mikayla.orange@bison.howard.edu>
2021-04-08 17:03:50 -0400

    initial commit

---
+++ DictionDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DictionDiary
+//
+//  Created by Mikayla Orange on 4/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
